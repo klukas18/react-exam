0bd5c614319a450307b0fef85359d66cc01716f9
Krzysiek <lukaszewicz.krzysztof91@gmail.com>
2024-02-06 17:42:47 +0100

    finished filtering logic for products

--- theshopper/src/App.jsx
+++ theshopper/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import ListActions from './components/ListActions/ListActions';
 import ProductsList from './components/ProductsList/ProductsList';
 import ShoppingList from './components/ShoppingList/ShoppingList';
@@ -9,6 +9,13 @@ import './App.css';
 
 function App() {
 	const [shoppingList, setShoppingList] = useState([]);
+	const [allProducts] = useState(products);
+	const [filteredProducts, setFilteredProducts] = useState(products);
+	const [name, setName] = useState('');
+	const [category, setCategory] = useState('');
+	const [isFood, setIsFood] = useState(false);
+
+	// Adding and removing elements from the shopping list
 
 	const addToShoppingList = (product) => {
 		setShoppingList((prevList) => {
@@ -24,12 +31,31 @@ function App() {
 			}
 		});
 	};
-
 	const removeFromShoppingList = (product) => {
 		setShoppingList((prevList) =>
 			prevList.filter((item) => item.name !== product.name)
 		);
 	};
+
+	// Filtering the products list
+
+	const categories = [...new Set(products.map((product) => product.category))];
+
+	const filterProducts = (products, name, category, isFood) => {
+		return products.filter(
+			(product) =>
+				(name
+					? product.name.toLowerCase().includes(name.toLowerCase())
+					: true) &&
+				(category ? product.category === category : true) &&
+				(isFood ? product.isFood === isFood : true)
+		);
+	};
+
+	useEffect(() => {
+		let filtered = filterProducts(allProducts, name, category, isFood);
+		setFilteredProducts(filtered);
+	}, [name, category, isFood]);
 
 	return (
 		<ShoppingListContext.Provider
@@ -39,8 +65,32 @@ function App() {
 				removeFromShoppingList,
 			}}>
 			<div className='columns'>
-				<ProductsList products={products} />
-				<ListActions />
+				<ProductsList products={filteredProducts} />
+				<div>
+					<input
+						type='text'
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						placeholder='Filter by name'
+					/>
+					<select
+						value={category}
+						onChange={(e) => setCategory(e.target.value)}>
+						<option value=''>All categories</option>
+						{categories.map((category, index) => (
+							<option key={index} value={category}>
+								{category}
+							</option>
+						))}
+					</select>
+					<input
+						type='checkbox'
+						checked={isFood}
+						onChange={(e) => setIsFood(e.target.checked)}
+					/>{' '}
+					Only food
+				</div>
+				{/* <ListActions /> */}
 				<ShoppingList />
 			</div>
 		</ShoppingListContext.Provider>
